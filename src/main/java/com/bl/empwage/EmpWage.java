aac37b65ca67891f18b9811f0cc159e1df6ef405
Kanishk <kanishksinghal06@gmail.com>
2023-04-12 15:08:45 +0530

    Added the code to calculate the wage for a month

--- src/main/java/com/bl/empwage/EmpWage.java
+++ src/main/java/com/bl/empwage/EmpWage.java
@@ -2,11 +2,18 @@ package com.bl.empwage;
 public class EmpWage {
     public static void main(String[] args) {
         System.out.println("Welcome to employee Wage Project");
+
+        // UC-1
+
             int a = (int)(Math.random() * 10) % 2;
-            if (a == 1)
+            if (a == 1) {
                 System.out.println("Employee is Present");
-            else
+            }
+                else {
                 System.out.println("Employee is absent");
+            }
+
+//          UC-2
 
         int IS_PRESENT = 1;
         int WAGE_PER_HOUR = 20;
@@ -19,6 +26,8 @@ public class EmpWage {
         } else {
             System.out.println("Employee is Absent. Daily Wage = " + dailyWage);
         }
+
+        // UC-3
 
         int IS_PART_TIME = 1;
         int IS_FULL_TIME = 2;
@@ -33,6 +42,8 @@ public class EmpWage {
         } else {
             System.out.println("Employee is Absent. Daily Wage = " + dailyWage);
         }
+
+        // UC-4
 
         final int PART_TIME = 1;
         final int FULL_TIME = 2;
@@ -52,5 +63,22 @@ public class EmpWage {
                 System.out.println("Employee is Absent. Daily Wage = " + dailyWage);
                 break;
         }
+
+//         UC-5
+        final int NUM_WORKING_DAYS = 20;
+        int monthlyWage = 0;
+
+        for (int i = 0; i < NUM_WORKING_DAYS; i++) {
+             NewAttendance = (int) (Math.random() * 3); // 0 for absent, 1 for part-time, 2 for full-time
+
+            dailyWage = switch (attendance) {
+                case PART_TIME -> WAGE_PER_HOUR * PART_TIME_HOUR;
+                case FULL_TIME -> WAGE_PER_HOUR * FULL_DAY_HOUR;
+                default -> 0;
+            };
+            monthlyWage += dailyWage;
+        }
+
+        System.out.println("Employee Monthly Wage = " + monthlyWage);
     }
 }
