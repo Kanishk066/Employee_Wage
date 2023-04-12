dd1adf0cfd14cd114c03bd41c7d888ba9594612b
Kanishk <kanishksinghal06@gmail.com>
2023-04-12 08:52:47 +0530

    Added the code to check the employee attendance

--- src/main/java/com/bl/empwage/EmpWage.java
+++ src/main/java/com/bl/empwage/EmpWage.java
@@ -2,6 +2,10 @@ package com.bl.empwage;
 public class EmpWage {
     public static void main(String[] args) {
         System.out.println("Welcome to employee Wage Project");
-
+            int a = (int)(Math.random() + 10) % 10;
+            if (a == 1)
+                System.out.println("Employee is Present");
+            else
+                System.out.println("Employee is absent");
     }
 }
